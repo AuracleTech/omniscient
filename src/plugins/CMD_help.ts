569bb4aae31a525403f2132a7689bbffe5f7f92d
Auracle <virgilfabre@outlook.com>
2022-02-10 17:53:21 -0500

    Plugin command help

---
+++ src/plugins/CMD_help.ts
@@ -0,0 +1,8 @@
+import { PrivateMessage } from 'bancho.js'
+import omniscient from '../omniscient'
+
+omniscient.on('pm', async (instance: PrivateMessage) => {
+    let cmd = instance.message.substring(1).split(' ')[0]
+    if (cmd === 'help' || cmd === 'h')
+        instance.user.sendMessage('[https://github.com/AuracleTech/omniscient omniscient on GitHub]')
+})
--- src/plugins/help.ts
+++
@@ -1,6 +0,0 @@
-import omniscient from '../omniscient'
-
-omniscient.on('pm', (instance: any) => {
-    if (instance.message.substring(1).split(' ')[0] === '!help')
-        return instance.user.sendMessage('[https://github.com/AuracleTech/omniscient see the GitHub page]')
-})
