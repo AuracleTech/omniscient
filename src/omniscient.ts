36c376fa828ad3fbcf268088f5f8b5ff58bf9c7e
Auracle <virgilfabre@outlook.com>
2022-02-10 17:52:34 -0500

    Adding event types

--- src/omniscient.ts
+++ src/omniscient.ts
@@ -1,16 +1,16 @@
+export type EventTypes = 'boot' | 'pm'
+
 export default class Events {
     private static events: { [key: string]: Function[] } = {}
 
-    static on(event: string, callback: Function): void {
-        if (!this.events[event]) {
+    static on(event: EventTypes, callback: Function): void {
+        if (!this.events[event])
             this.events[event] = []
-        }
         this.events[event].push(callback)
     }
 
-    static emit(event: string, data: any): void {
-        if (this.events[event]) {
+    static emit(event: EventTypes, data?: any): void {
+        if (this.events[event])
             this.events[event].forEach(callback => callback(data))
-        }
     }
 }
